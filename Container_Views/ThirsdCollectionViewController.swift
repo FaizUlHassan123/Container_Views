a9091f5cec020fc4d80d692322721d149da0ce7f
Faiz Ul Hassan <faizhassan550@gmail.com>
2022-06-25 14:56:55 +0500

    Add files

---
+++ Container_Views/ThirsdCollectionViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ThirsdCollectionViewController.swift
+//  Container_Views
+//
+//  Created by Faiz Ul Hassan on 6/25/22.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class ThirsdCollectionViewController: UICollectionViewController {
+
+    let dummyDataArray : [String] = ["Gilgit", "Pakistan", "SIalkot", "Europe", "Malaysia", "INDIA" , "USA", "UK", "Autralia", "Bangladesh", "Russia" , "Ukraine","Dilhi","Punjab","Saudia Arabia","SINDH", "Pakistan", "SIalkot", "Europe", "Malaysia", "INDIA" , "USA", "UK", "Autralia", "Bangladesh", "Russia" , "Ukraine","Dilhi","Punjab","Saudia Arabia","SINDH", "Pakistan", "SIalkot", "Europe", "Malaysia", "INDIA" , "USA", "UK", "Autralia", "Bangladesh", "Russia" , "Ukraine","Dilhi","Punjab","Saudia Arabia","SINDH", "Pakistan", "SIalkot", "Europe", "Malaysia", "INDIA" , "USA", "UK", "Autralia", "Bangladesh", "Russia" , "Ukraine","Dilhi","Punjab","Saudia Arabia","SINDH"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+       // self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return dummyDataArray.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let data = dummyDataArray[indexPath.row]
+
+        if let label = cell.viewWithTag(399) as? UILabel{
+            label.text = data
+        }
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
--- Container_Views/ViewController.swift
+++ Container_Views/ViewController.swift
@@ -9,6 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var first_View:UIView!
+    @IBOutlet weak var second_view:UIView!
+
+    //Add Tablview from other VIewcontroller
+    private lazy var FirstObject: SecondTableViewController = {
+        // Instantiate View Controller
+          let storyboard = UIStoryboard(name: "Main", bundle: nil)
+          let viewController = storyboard.instantiateViewController(withIdentifier: "SecondTableViewController") as! SecondTableViewController
+        return viewController
+      }()
+    // Adding it as Subview:
+     private func add(asChildViewController viewController: UIViewController){
+        // Configure Child View
+        viewController.view.frame = CGRect(x: 0, y: 0, width: self.first_View.frame.size.width, height: self.first_View.frame.size.height)
+
+        // Add Child View Controller
+        addChild(viewController)
+        viewController.view.translatesAutoresizingMaskIntoConstraints = true
+
+        // Add Child View as Subview
+         first_View.addSubview(viewController.view)
+
+        // Notify Child View Controller
+        viewController.didMove(toParent: self)
+    }
+
+    //Add Tablview from other VIewcontroller
+    private lazy var SecondtObject: ThirsdCollectionViewController = {
+        // Instantiate View Controller
+          let storyboard = UIStoryboard(name: "Main", bundle: nil)
+          let viewController = storyboard.instantiateViewController(withIdentifier: "ThirsdCollectionViewController") as! ThirsdCollectionViewController
+        return viewController
+      }()
+    // Adding it as Subview:
+     private func add2(asChildViewController viewController: UIViewController){
+        // Configure Child View
+        viewController.view.frame = CGRect(x: 0, y: 0, width: self.second_view.frame.size.width, height: self.second_view.frame.size.height)
+
+        // Add Child View Controller
+        addChild(viewController)
+        viewController.view.translatesAutoresizingMaskIntoConstraints = true
+
+        // Add Child View as Subview
+         second_view.addSubview(viewController.view)
+
+        // Notify Child View Controller
+        viewController.didMove(toParent: self)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.add(asChildViewController: FirstObject)
+        self.add2(asChildViewController: SecondtObject)
+    }
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
